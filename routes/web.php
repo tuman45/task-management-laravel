4bb5b5262399dd2710f7af87888f9fdb8741e46d
tuman45 <qblmahmudi@gmail.com>
2023-12-07 22:48:26 +0700

    create view for test template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+Route::get('/task', function () {
+    return view('task', [
+        "title" => "Task"
+    ]);
+});
+Route::get('/user', function () {
+    return view('user', [
+        "title" => "User"
+    ]);
 });
