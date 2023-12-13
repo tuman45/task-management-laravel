f98cefd1f4ef87d08c1eb95e232ed6b8c917a115
tuman45 <qblmahmudi@gmail.com>
2023-12-13 23:48:50 +0700

    Added a new method to show all tasks based on a board list

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Board;
+use App\Models\Board_list;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
+    public function showTask(Board $board)
+    {
+        // Get all tasks related to the board
+        $tasks = Task::with(['board', 'board_list'])->where('board_id', $board->id)->get();
+
+        // Group tasks by board_list_id
+        $groupedTasks = $tasks->groupBy('board_list_id');
+
+        // Get board_lists in the desired order
+        $boardLists = Board_list::where('board_id', $board->id)->orderBy('order')->get();
+
+        return view('tasks', [
+            "title" => "Tasks",
+            "board" => $board,
+            "groupedTasks" => $groupedTasks,
+            "boardLists" => $boardLists,
+        ]);
+    }
     public function showDetail(Board $board, Task $task)
     {
         return view('detail', [
