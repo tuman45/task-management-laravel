17b73d53657de5eab17c26d24f76442db6988206
tuman45 <qblmahmudi@gmail.com>
2023-12-14 16:21:35 +0700

    Change route to board using resource

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,23 @@ use App\Http\Controllers\RegisterController;
 |
 */
 
-Route::get('/', [BoardController::class, 'index'])->middleware('auth');
+// Route::get('/', [BoardController::class, 'index'])->middleware('auth');
+
+Route::resource('/', BoardController::class)->middleware('auth');
+
+Route::get('/board/checkSlug', [BoardController::class, 'checkSlug'])->middleware('auth');
+Route::resource('board', BoardController::class)->middleware('auth');
 
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
 
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
+// routes/web.php
 
-Route::get('/{board:board_slug}', [TaskController::class, 'showTask'])->middleware('auth');
-Route::get('/{board:board_slug}/{task:task_slug}', [TaskController::class, 'showDetail'])->middleware('auth');
+Route::resource('{board_slug}', TaskController::class)->middleware('auth');
+Route::resource('{board_slug}/{task_slug}', TaskController::class)->middleware('auth');
